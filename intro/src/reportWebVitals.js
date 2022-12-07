455c8d10b41b1085e3a62195705dfe872c5db618
Oktay Berk Altıntaş <65086193+oktayberk@users.noreply.github.com>
2022-12-07 20:13:48 +0300

    Update reportWebVitals.js

--- intro/src/reportWebVitals.js
+++ intro/src/reportWebVitals.js
@@ -1,42 +1,13 @@
-import React, { Component } from "react";
-import { ListGroup, ListGroupItem } from "reactstrap";
-
-export default class CategoryList extends Component {
-  state = {
-    categories: [],
-  };
-
-  componentDidMount() {
-    this.getCategories();
+const reportWebVitals = onPerfEntry => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
   }
+};
 
-  getCategories = () => {
-    fetch("http://localhost:3000/categories")
-      .then((response) => response.json())
-      .then((data) => this.setState({ categories: data }));
-  };
-
-  render() {
-    return (
-      <div>
-        <h3>{this.props.info.title}</h3>
-        <ListGroup>
-          {this.state.categories.map((category) => (
-            <ListGroupItem
-              active={
-                category.categoryName === this.props.currentCategory
-                  ? true
-                  : false
-              }
-              onClick={() => this.props.changeCategory(category)}
-              key={category.id}
-            >
-              {category.categoryName}
-            </ListGroupItem>
-          ))}
-        </ListGroup>
-        {/* <h4>{this.props.currentCategory}</h4>{" "} */}
-      </div>
-    );
-  }
-}
+export default reportWebVitals;
